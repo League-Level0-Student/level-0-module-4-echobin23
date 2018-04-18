b18f74691da69e8ea0575e9086caf8398c6e8dad
Vivicoolcat1 <league@WTS19.attlocal.net>
2018-04-17 18:29:46 -0700

    change calculator, testscores

--- src/ChangeCalculator.java
+++ src/ChangeCalculator.java
@@ -14,17 +14,19 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String answer = JOptionPane.showInputDialog("how many nickels do you have");
 		// Convert their answer to an int using Integer.parseInt()
-
+int nickels = Integer.parseInt(answer);
 		// Ask the user how many dimes they have, and convert their answer
-
-		// Ask the user how many quarters they have, and convert their answer
-
+String numbers = JOptionPane.showInputDialog("how many dimes do you have");
+int dimes = Integer.parseInt(numbers);
+       // Ask the user how many quarters they have, and convert their answer
+String coins = JOptionPane.showInputDialog("how many quarters do you have");
+int quarters = Integer.parseInt(coins);
 		// Calculate how much money the user has and save it in a double variable 
-
+double total = (nickels*0.05)+(dimes*0.1)+(quarters*0.25);
 		// Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null, "You have $"+total+".");
 	}
 }
 
---
+++ src/extra/Testscores.java
@@ -0,0 +1,11 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class Testscores {
+	public static void main(String[] args) {
+	String answer = JOptionPane.showInputDialog("What was your test score");
+double score =	Double.parseDouble(answer);
+	
+	}
+}
