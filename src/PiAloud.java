d10f750005b891f034b3e58255f283aa3b2db84d
echobin23 <league@WTS14-4.attlocal.net>
2018-05-01 18:27:40 -0700

    PiAloud, HappyPet IP

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -1,29 +1,41 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
-import java.io.IOException;
 import java.util.Scanner;
 
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
 	public static void main(String[] args) {
-	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-String pi = "3.14159";
-	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-pi.charAt(0);
-	// 4. Print ALL the digits of of Pi (hint: use a loop)
-for(int i =0; i > pi.charAt(0); i++) {
-	
-}
-	// 5. Use the speak() method to speak all the digits of Pi.
+		// 2. Make a String variable to hold the value of Pi. You could use
+		// http://www.piday.org/million/ for the value.
+		String pi = "3.141592653";
+		// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second
+		// is "pi.charAt(1)"
+		System.out.println(pi.charAt(0));
+		System.out.println(pi.charAt(1));
+		System.out.println(pi.charAt(2));
+		System.out.println(pi.charAt(3));
+		System.out.println(pi.charAt(4));
+		System.out.println(pi.charAt(5));
+		System.out.println(pi.charAt(6));
+		System.out.println(pi.charAt(7));
 
-	// [ADVANCED]
-	// *6. Get a character from the user using the getInputFromUser() method
-	// *7. Compare the users' char to the next digit of Pi
-	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
+		// 4. Print ALL the digits of of Pi (hint: use a loop)
+		for (int i = 0; i < pi.length(); i++) {
+			speak(pi.charAt(i));
+
+		}
+		// 5. Use the speak() method to speak all the digits of Pi.
+
+		// [ADVANCED]
+		// *6. Get a character from the user using the getInputFromUser() method
+		// *7. Compare the users' char to the next digit of Pi
+		// *8. If they are correct, print out "correct". If they are not, print
+		// "incorrect" to System.err.println
 	}
+
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
@@ -40,12 +52,9 @@ for(int i =0; i > pi.charAt(0); i++) {
 		}
 	}
 
-
 	static char getInputFromUser() {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.next().toCharArray()[0];
 	}
 
 }
-
-
